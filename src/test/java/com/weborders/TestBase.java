a4d1a154354a013e453059a5048e2a8262f11921
Firuz Khalimov <fkhalimov22@gmail.com>
2020-07-10 08:36:48 -0400

    first commit

---
+++ src/test/java/com/weborders/TestBase.java
@@ -0,0 +1,33 @@
+package com.weborders;
+
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+
+public abstract class TestBase {
+
+    @BeforeMethod
+    public void setUp(){
+
+        String URL = ConfigurationReader.getProperty("url");
+
+        Driver.getDriver().get(URL);
+
+        Driver.getDriver().manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void teraDown(){
+
+        Driver.closeDriver();
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/weborders/tests/LoginTests.java
@@ -0,0 +1,22 @@
+package com.weborders.tests;
+
+import com.weborders.TestBase;
+import com.weborders.pages.LoginPage;
+import com.weborders.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+@Test(description = "Just log in and verify title")
+    public void loginTest(){
+
+    LoginPage loginPage = new LoginPage();
+
+    loginPage.login();
+
+    Assert.assertEquals(Driver.getDriver().getTitle(), "Web Orders", "Page title is wrong!");
+
+}
+
+}
